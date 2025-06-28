fa3f01b6da815eda86d8da2f8987850703ee83c8
Anirudh21165 <it21165@glbitm.ac.in>
2025-06-28 17:01:43 +0530

    optimal approch with extra space

--- Arrays/Leetcode Easy/Find Subsequence of length k with maxsum.cpp
+++ Arrays/Leetcode Easy/Find Subsequence of length k with maxsum.cpp
@@ -24,10 +24,46 @@ vector<int>maxsubsequence(vector<int> &nums , int k){
 int main(){
     vector<int>nums={-1,-2,-10,-11,5,4,9,7};
     int k=4;
-    cout<<"hello";
     vector<int>result=maxsubsequence(nums,k);
     for(int i=0;i<k;i++){
         cout<<result[i]<<" ";
     }
     return 0;
 }
+
+//Approach-2 (Using nth_element for partial sort)
+//T.C : Average O(n+k)
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        if (k == nums.size())
+            return nums;
+
+        // Copy nums to a temp vector to find k largest elements
+        vector<int> temp(nums);
+        
+        // Use nth_element to put the k largest elements in the first k positions (unordered)
+        nth_element(begin(temp), begin(temp) + k - 1, end(temp), greater<int>()); //Average TC is O(n)
+
+        // Determine the threshold value (k-th largest element)
+        int threshold = temp[k - 1];
+
+        // Count how many times threshold appears in top k elements
+        int countThreshold = count(temp.begin(), temp.begin() + k, threshold); //O(k)
+
+        vector<int> result;
+        for (int num : nums) {
+            if (num > threshold) {
+                result.push_back(num);
+            } else if (num == threshold && countThreshold > 0) {
+                result.push_back(num);
+                --countThreshold;
+            }
+            if (result.size() == k)
+                break;
+        }
+
+        return result;
+    }
+};
